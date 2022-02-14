fd6b7aa6d16dec8e47a77b55b490712e27325e71
Doodles <doodles33@protonmail.com>
2022-02-14 20:19:53 -0300

    Bootstrap project frontend app

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+export const App = () => {
+  return (
+    <div>
+      <h1>BuildUp App</h1>
+      <p>A website to track notes, habits and become a better person.</p>
+    </div>
+  );
+};
+
+export default App;
