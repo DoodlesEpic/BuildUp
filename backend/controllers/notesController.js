3e114b9f0f0bcfe7aa5be62d28273da60780915f
Doodles <doodles33@protonmail.com>
2022-02-14 04:04:22 -0300

    Note controller create note validation and async everything

--- backend/controllers/notesController.js
+++ backend/controllers/notesController.js
@@ -1,38 +1,45 @@
+const asyncHandler = require("express-async-handler");
+
 /**
  * @desc  Get all notes
  * @route  GET /api/notes
  * @access Private
  */
-const getNotes = (req, res) => {
+const getNotes = asyncHandler(async (req, res) => {
   res.json({ message: "Get notes" });
-};
+});
 
 /**
  * @desc  Create a note
  * @route  POST /api/notes
  * @access Private
  */
-const createNote = (req, res) => {
+const createNote = asyncHandler(async (req, res) => {
+  if (!req.body.title || !req.body.content) {
+    res.status(400);
+    throw new Error("Please provide title and content");
+  }
+
   res.json({ message: "Create note" });
-};
+});
 
 /**
  * @desc  Update a note
  * @route  PUT /api/notes/:id
  * @access Private
  */
-const updateNote = (req, res) => {
+const updateNote = asyncHandler(async (req, res) => {
   res.json({ message: `Update note ${req.params.id}` });
-};
+});
 
 /**
  * @desc  Delete a note
  * @route  DELETE /api/notes/:id
  * @access Private
  */
-const deleteNote = (req, res) => {
+const deleteNote = asyncHandler(async (req, res) => {
   res.json({ message: `Delete note ${req.params.id}` });
-};
+});
 
 module.exports = {
   getNotes,
