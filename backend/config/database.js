65b9c9ca05c7f94ab0c9004f1804c95811855043
Doodles <doodles33@protonmail.com>
2022-02-14 04:22:22 -0300

    Database connection

---
+++ backend/config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+
+const connectToDatabase = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Connected to database ${connection.connections[0].name}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+module.exports = { connectToDatabase };
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
+const { connectToDatabase } = require("./config/database");
 const port = process.env.PORT || 5000;
+
+// Initialize database connection
+connectToDatabase();
 
 // Initialize express
 const app = express();
