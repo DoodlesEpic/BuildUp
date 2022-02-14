77d780b1b668d3de6f8916019b9cae1535af2340
Doodles <doodles33@protonmail.com>
2022-02-14 03:37:42 -0300

    Initial server code

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log("BuildUp started");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/api/habits", (req, res) => {
+  res.json({ message: "Get habits" });
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json({ message: "Get notes" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
