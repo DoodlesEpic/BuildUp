af61ac38ca2a297a7e16a54e04b9ac6c0ca44a69
Doodles <doodles33@protonmail.com>
2022-02-14 19:30:34 -0300

    Implement authentication middleware

---
+++ backend/middleware/authenticationMiddleware.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  // Check if the token is in the header
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // Get the token from the header
+      token = req.headers.authorization.split(" ")[1];
+
+      // Verify the token
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Add the user from the token to the request
+      req.user = await User.findById(decodedToken.id);
+
+      // Continue to the next middleware
+      next();
+
+      // If the token is not valid
+    } catch (error) {
+      res.status(401);
+      throw new Error("Not authorized");
+    }
+  }
+
+  // Make sure the user is logged in
+  if (!token) {
+    res.status(401);
+    throw new Error("You must be logged in to access this route");
+  }
+});
+
+module.exports = { protect };
