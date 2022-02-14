f8fc40f3b00a88d4dc44705dd00974fda6157080
Doodles <doodles33@protonmail.com>
2022-02-14 03:55:07 -0300

    Handle body data in server

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,17 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
+// Initialize express
 const app = express();
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes middleware
 app.use("/api/notes", require("./routes/notesRoutes"));
 
+// Start server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
