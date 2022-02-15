91d2c7e85987d2cf0350d9b79d284265a02001fe
Doodles <doodles33@protonmail.com>
2022-02-14 22:53:30 -0300

    Register and login pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,60 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+
+  // State
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+
+  // Handle Input Change
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    navigate("/");
+  };
+
+  // Render
+  return (
+    <>
+      <section>
+        <h1>Login</h1>
+      </section>
+      <section>
+        <form onSubmit={onSubmitForm}>
+          <div>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="email@example.com"
+              onChange={onChange}
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Password"
+              onChange={onChange}
+            />
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,71 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Register = () => {
-  return <div>Register</div>;
+  const navigate = useNavigate();
+
+  // State
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const { username, email, password } = formData;
+
+  // Handle Input Change
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    navigate("/login");
+  };
+
+  // Render
+  return (
+    <>
+      <section>
+        <h1>Register</h1>
+      </section>
+      <section>
+        <form onSubmit={onSubmitForm}>
+          <div>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              placeholder="Username"
+              onChange={onChange}
+            />
+          </div>
+          <div>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="email@example.com"
+              onChange={onChange}
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Password"
+              onChange={onChange}
+            />
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default Register;
