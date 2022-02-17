84cd64dc75209bc970fe92c62ad151bf721dedba
Doodles <doodles33@protonmail.com>
2022-02-17 18:54:38 -0300

    Remove MantineProvider from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AppShell, Navbar, Header, MantineProvider } from "@mantine/core";
+import { AppShell, Navbar, Header } from "@mantine/core";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -8,35 +8,33 @@ import HeaderContent from "./components/HeaderContent";
 export const App = () => {
   return (
     <Router>
-      <MantineProvider>
-        <AppShell
-          padding="md"
-          navbar={
-            <Navbar width={{ base: 300 }} height={500} padding="xs">
-              {/* Navbar content */}
-            </Navbar>
-          }
-          header={
-            <Header height={60} padding="xs">
-              {<HeaderContent />}
-            </Header>
-          }
-          styles={(theme) => ({
-            main: {
-              backgroundColor:
-                theme.colorScheme === "dark"
-                  ? theme.colors.dark[8]
-                  : theme.colors.gray[0],
-            },
-          })}
-        >
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </AppShell>
-      </MantineProvider>
+      <AppShell
+        padding="md"
+        navbar={
+          <Navbar width={{ base: 300 }} height={500} padding="xs">
+            {/* Navbar content */}
+          </Navbar>
+        }
+        header={
+          <Header height={60} padding="xs">
+            {<HeaderContent />}
+          </Header>
+        }
+        styles={(theme) => ({
+          main: {
+            backgroundColor:
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[8]
+                : theme.colors.gray[0],
+          },
+        })}
+      >
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </AppShell>
     </Router>
   );
 };
