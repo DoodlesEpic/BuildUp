8d048743f0c4c01c7672a4b19896f03f2a1f0f3d
Doodles <doodles33@protonmail.com>
2022-02-23 13:07:45 -0300

    Add habit days routes to habit routes

--- backend/routes/habitsRoutes.js
+++ backend/routes/habitsRoutes.js
@@ -2,15 +2,23 @@ const express = require("express");
 const router = express.Router();
 const {
   getHabits,
+  getHabitDays,
   createHabit,
+  createHabitDay,
   updateHabit,
+  updateHabitDay,
   deleteHabit,
+  deleteHabitDay,
 } = require("../controllers/habitsController");
 const { protect } = require("../middleware/authenticationMiddleware");
 
 router.get("/", protect, getHabits);
+router.get("/:id", protect, getHabitDays);
 router.post("/", protect, createHabit);
+router.post("/:id", protect, createHabitDay);
 router.put("/:id", protect, updateHabit);
+router.put("/:id/:day", protect, updateHabitDay);
 router.delete("/:id", protect, deleteHabit);
+router.delete("/:id/:day", protect, deleteHabitDay);
 
 module.exports = router;
