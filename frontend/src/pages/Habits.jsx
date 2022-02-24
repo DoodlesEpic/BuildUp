f04bd7b2988d5bd9b7bddeb3bc65256e25e0040f
Doodles <doodles33@protonmail.com>
2022-02-24 18:10:44 -0300

    Implement habit listing in habits page

--- frontend/src/pages/Habits.jsx
+++ frontend/src/pages/Habits.jsx
@@ -1,6 +1,43 @@
 import { Container, Text, Title } from "@mantine/core";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import HabitItem from "../components/HabitItem";
+import Loading from "../components/Loading";
+import { getHabits } from "../features/habits/habitsSlice";
 
 const Habits = () => {
+  // Initialize hooks
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  // Auth State
+  const user = useSelector((state) => state.authentication.user);
+
+  // Notes State
+  const { habits, isLoading, isError, message } = useSelector(
+    (state) => state.habits
+  );
+
+  useEffect(() => {
+    dispatch(getHabits());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (!user) {
+      navigate("/");
+    }
+  }, [user, navigate, isError, message, dispatch]);
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+
   return (
     <Container>
       <section>
@@ -9,6 +46,13 @@ const Habits = () => {
           Track your habits, mantain a consistent dailly streak for 90 days to
           form a habit
         </Text>
+      </section>
+      <section>
+        <ul>
+          {habits.map((habit) => (
+            <HabitItem key={habit._id} habit={habit} />
+          ))}
+        </ul>
       </section>
     </Container>
   );
