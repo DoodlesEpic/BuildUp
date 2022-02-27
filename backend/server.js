b368eef906afe471cfd12e36391449173bd9afac
Doodles <doodles33@protonmail.com>
2022-02-27 14:24:23 -0300

    Add helmet to the backend for improved security

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const helmet = require("helmet");
 const path = require("path");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
@@ -12,6 +13,7 @@ connectToDatabase();
 const app = express();
 
 // Middleware
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
