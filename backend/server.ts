f0292e0ae0ec1ccc59dc0da815a8cbb7787d780e
Doodles <moraes.eduardo@proton.me>
2023-02-16 20:11:18 -0300

    fix: incorrect relative path for express static

--- backend/server.ts
+++ backend/server.ts
@@ -30,7 +30,7 @@ app.use("/api/habits", habitsRouter);
 
 // Serve frontend
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"))
   );
