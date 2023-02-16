85043467305146fd7d0f48003f1d26bc37e08e88
Doodles <moraes.eduardo@proton.me>
2023-02-16 20:08:04 -0300

    fix: MONGO_URI must be defined always being thrown

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,11 +1,9 @@
 import mongoose from "mongoose";
 
 export const connectToDatabase = async () => {
-  if (process.env.MONGO_URI) {
-    mongoose.set("strictQuery", false);
-    const connection = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`Connected to database ${mongoose.connection.name}`);
-  }
+  if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
 
-  throw new Error("MONGO_URI must be defined");
+  mongoose.set("strictQuery", false);
+  const connection = await mongoose.connect(process.env.MONGO_URI);
+  console.log(`Connected to database ${mongoose.connection.name}`);
 };
