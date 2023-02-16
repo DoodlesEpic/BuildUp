0a24c6a9f674bb3f4c0c556189d3c3a0c977a333
Doodles <moraes.eduardo@proton.me>
2023-02-16 02:38:35 -0300

    fix: duplicated form import in noteform.jsx

--- frontend/src/components/NoteForm.jsx
+++ frontend/src/components/NoteForm.jsx
@@ -2,7 +2,7 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { Form, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const NoteForm = ({ submitDispatch, note }) => {
   // Initialize hooks
