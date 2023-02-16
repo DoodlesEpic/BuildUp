0e081150389856f12534edb5dae81df14de013ea
Doodles <moraes.eduardo@proton.me>
2023-02-16 00:52:31 -0300

    fix: move proxy settings from cra to vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [react()],
 });
