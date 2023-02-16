58c4fb65b09836ce8b80d9652ed7a40f758e283d
Doodles <moraes.eduardo@proton.me>
2023-02-16 15:22:45 -0300

    feat: add navbar and fluid container to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import ThemeProvider from "react-bootstrap/Nav";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
@@ -11,34 +12,45 @@ import Register from "./pages/Register";
 import Notes from "./pages/Notes";
 import EditNote from "./pages/EditNote";
 import Habits from "./pages/Habits";
+import { Container, Row } from "react-bootstrap";
 
 export const App = () => {
   return (
     <Router>
       <ThemeProvider>
-        <Nav>
-          <Nav.Item>
-            <Nav.Link href="/habits">Habits</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="/notes">Notes</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="/login">Login</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="/register">Register</Nav.Link>
-          </Nav.Item>
-        </Nav>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/habits" element={<Habits />} />
-          <Route path="/notes" element={<Notes />} />
-          <Route path="/notes/:id" element={<EditNote />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-        <ToastContainer />
+        <Container fluid>
+          <Navbar>
+            <Navbar.Brand href="/">BuildUp</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse>
+              <Nav>
+                <Nav.Item>
+                  <Nav.Link href="/habits">Habits</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href="/notes">Notes</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href="/login">Login</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href="/register">Register</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/habits" element={<Habits />} />
+            <Route path="/notes" element={<Notes />} />
+            <Route path="/notes/:id" element={<EditNote />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+
+          <ToastContainer />
+        </Container>
       </ThemeProvider>
     </Router>
   );
