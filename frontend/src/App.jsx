853d7ba96f6a17963a93f63b73a657df3b8a4d39
Doodles <moraes.eduardo@proton.me>
2023-02-16 16:47:58 -0300

    feat: show navlinks based on logged in state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import ThemeProvider from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -12,9 +14,11 @@ import Register from "./pages/Register";
 import Notes from "./pages/Notes";
 import EditNote from "./pages/EditNote";
 import Habits from "./pages/Habits";
-import { Container, Row } from "react-bootstrap";
 
 export const App = () => {
+  // Auth State
+  const { user } = useSelector((state) => state.authentication);
+
   return (
     <Router>
       <ThemeProvider>
@@ -24,18 +28,28 @@ export const App = () => {
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse>
               <Nav>
-                <Nav.Item>
-                  <Nav.Link href="/habits">Habits</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link href="/notes">Notes</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link href="/login">Login</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link href="/register">Register</Nav.Link>
-                </Nav.Item>
+                {user ? (
+                  <>
+                    <Nav.Item>
+                      <Nav.Link href="/habits">Habits</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link href="/notes">Notes</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link href="/notes">Notes</Nav.Link>
+                    </Nav.Item>
+                  </>
+                ) : (
+                  <>
+                    <Nav.Item>
+                      <Nav.Link href="/login">Login</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link href="/register">Register</Nav.Link>
+                    </Nav.Item>
+                  </>
+                )}
               </Nav>
             </Navbar.Collapse>
           </Container>
