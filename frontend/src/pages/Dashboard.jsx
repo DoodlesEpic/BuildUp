4fadb257d2e92a2f580822171d999886365d6e69
Doodles <moraes.eduardo@proton.me>
2023-02-16 02:42:38 -0300

    feat: migrate dashboard.jsx to bootstrap

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,15 +1,11 @@
-import { Container, Title, Text, Center } from "@mantine/core";
+import Container from "react-bootstrap/Container";
 
 const Dashboard = () => {
   return (
-    <Center mt="xl" style={{ height: "80%" }}>
-      <Container>
-        <Title>BuildUp App</Title>
-        <Text>
-          A website to track notes, habits and become a better person.
-        </Text>
-      </Container>
-    </Center>
+    <Container>
+      <h1>BuildUp App</h1>
+      <p>A website to track notes, habits and become a better person.</p>
+    </Container>
   );
 };
 
