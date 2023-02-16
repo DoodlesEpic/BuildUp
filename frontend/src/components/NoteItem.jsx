b495962d08debb0623d61bce932ad8d535bb7ba7
Doodles <moraes.eduardo@proton.me>
2023-02-16 16:00:09 -0300

    feat: use cards for noteitem

--- frontend/src/components/NoteItem.jsx
+++ frontend/src/components/NoteItem.jsx
@@ -1,32 +1,37 @@
 import Form from "react-bootstrap/Form";
-import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Anchor from "react-bootstrap/Anchor";
 
 import { HiPencil, HiTrash } from "react-icons/hi";
 import { useDispatch } from "react-redux";
 import { deleteNote } from "../features/notes/notesSlice";
+import { Stack } from "react-bootstrap";
 
 const NoteItem = ({ note }) => {
   // Initialize hooks
   const dispatch = useDispatch();
 
   return (
-    <Container>
-      <p>{note.title}</p>
-      <p>{note.content}</p>
-      <Form.Group>
-        <p>{new Date(note.updatedAt).toLocaleString("en-US")}</p>
+    <Card className="mt-3">
+      <Card.Body>
+        <Card.Title>{note.title}</Card.Title>
+        <Card.Text>{note.content}</Card.Text>
         <Form.Group>
-          <Button as={Anchor} href={`/notes/${note._id}`}>
-            <HiPencil />
-          </Button>
-          <Button onClick={() => dispatch(deleteNote(note))}>
-            <HiTrash />
-          </Button>
+          <Card.Text>
+            {new Date(note.updatedAt).toLocaleString("en-US")}
+          </Card.Text>
+          <Form.Group as={Stack} direction="horizontal" gap={1}>
+            <Button as={Anchor} href={`/notes/${note._id}`}>
+              <HiPencil />
+            </Button>
+            <Button onClick={() => dispatch(deleteNote(note))}>
+              <HiTrash />
+            </Button>
+          </Form.Group>
         </Form.Group>
-      </Form.Group>
-    </Container>
+      </Card.Body>
+    </Card>
   );
 };
 
