97fe29ac7416689f7d9866e8d4b1d96dccfe7cf2
Doodles <moraes.eduardo@proton.me>
2023-02-15 23:55:30 -0300

    fix: DeprecationWarning Mongoose strictQuery
    Use the new default of false for strictQuery for mongoose
    https://www.mongodb.com/community/forums/t/deprecationwarning-mongoose-the-strictquery/209637/1

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
   try {
+    mongoose.set("strictQuery", false);
     const connection = await mongoose.connect(process.env.MONGO_URI);
     console.log(`Connected to database ${connection.connections[0].name}`);
   } catch (error) {
