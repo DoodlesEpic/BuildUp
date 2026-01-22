4b9e7f312a1e89461e72affc9dd144d1caeb5605
Doodles <moraes.eduardo@proton.me>
2026-01-22 10:35:45 -0300

    fix: cast JWT_EXPIRES_IN to a StringValue

--- backend/src/controllers/usersController.ts
+++ backend/src/controllers/usersController.ts
@@ -141,6 +141,6 @@ const generateToken = (id: number) => {
     throw new Error("JWT_EXPIRES_IN must be defined");
 
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
+    expiresIn: process.env.JWT_EXPIRES_IN as jwt.SignOptions["expiresIn"],
   });
 };
